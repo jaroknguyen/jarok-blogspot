3727ad4c8fd8d79044ca26dff208b2590f1751c2
Chi Nguyen <nquocchi@gmail.com>
2022-11-25 15:21:01 +0700

    updated nextjs default generation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/tailwind.css' // import Tailwind globally
+import Layout from '../components/Layout' // header layout persists between page changes
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }) {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
+export default MyApp
